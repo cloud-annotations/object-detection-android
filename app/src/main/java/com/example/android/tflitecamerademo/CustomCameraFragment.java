a6d3a87e3b4b2f39f0f87b16d41a056f9b5e6ce6
Nick Bourdakos <bourdakos1@gmail.com>
2019-04-13 20:45:40 -0400

    add permissions prompt

--- app/src/main/java/com/example/android/tflitecamerademo/CustomCameraFragment.java
+++ app/src/main/java/com/example/android/tflitecamerademo/CustomCameraFragment.java
@@ -1,5 +1,7 @@
 package com.example.android.tflitecamerademo;
 
+import android.Manifest;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -7,8 +9,11 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.RectF;
+import android.hardware.display.DisplayManager;
 import android.media.ThumbnailUtils;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Display;
 import android.view.LayoutInflater;
@@ -22,16 +27,19 @@ import android.widget.LinearLayout;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.xlythe.fragment.camera.CameraFragment;
 import com.xlythe.view.camera.CameraView;
+import com.xlythe.view.camera.PermissionChecker;
 
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -39,13 +47,43 @@ import androidx.recyclerview.widget.RecyclerView;
 import static com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_EXPANDED;
 import static com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_HIDDEN;
 
-public class CustomCameraFragment extends CameraFragment {
+public class CustomCameraFragment extends Fragment {
+    private static final String[] REQUIRED_PERMISSIONS;
+    private static final String[] OPTIONAL_PERMISSIONS;
+    private static final int REQUEST_CODE_PERMISSIONS = 10;
+    static {
+        // In KitKat+, WRITE_EXTERNAL_STORAGE is optional
+        if (Build.VERSION.SDK_INT >= 19) {
+            REQUIRED_PERMISSIONS = new String[] {
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.RECORD_AUDIO
+            };
+            OPTIONAL_PERMISSIONS = new String[] {
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.VIBRATE
+            };
+        } else {
+            REQUIRED_PERMISSIONS = new String[] {
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.RECORD_AUDIO,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            };
+            OPTIONAL_PERMISSIONS = new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.VIBRATE
+            };
+        }
+    }
+
     private ImageClassifier mClassifier;
 
     private CameraView mCamera;
     private ImageView mPreview;
     private ViewGroup mCameraLayout;
     private ViewGroup mPreviewLayout;
+    private View mPermissionPrompt;
+    private View mPermissionRequest;
     private Button mCapture;
     private Button mClose;
 
@@ -105,6 +143,10 @@ public class CustomCameraFragment extends CameraFragment {
 
     private View.OnClickListener mClosePreview = (view) -> showCamera();
 
+    private View.OnClickListener mPermissionListener = (view) -> requestPermissions(
+            concat(REQUIRED_PERMISSIONS, OPTIONAL_PERMISSIONS), REQUEST_CODE_PERMISSIONS
+    );
+
     private void showPreview() {
         mShowCamera = false;
         mCameraLayout.setVisibility(View.GONE);
@@ -115,6 +157,24 @@ public class CustomCameraFragment extends CameraFragment {
         mShowCamera = true;
         mCameraLayout.setVisibility(View.VISIBLE);
         mPreviewLayout.setVisibility(View.GONE);
+        mPermissionPrompt.setVisibility(View.GONE);
+    }
+
+    private void showPermissionPrompt() {
+        mCameraLayout.setVisibility(View.GONE);
+        mPreviewLayout.setVisibility(View.GONE);
+        mPermissionPrompt.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (PermissionChecker.hasPermissions(getContext(), REQUIRED_PERMISSIONS)) {
+                showCamera();
+            } else {
+                showPermissionPrompt();
+            }
+        }
     }
 
     @Override
@@ -142,35 +202,44 @@ public class CustomCameraFragment extends CameraFragment {
         mPreviewLayout = view.findViewById(R.id.layout_preview);
         mCapture = view.findViewById(R.id.capture_bitmap);
         mClose = view.findViewById(R.id.close_preview);
+        mPermissionPrompt = view.findViewById(R.id.layout_permissions);
+        mPermissionRequest = view.findViewById(R.id.request_permissions);
+
+        mCamera.open();
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
         mCapture.setOnClickListener(mBitmapCaptured);
         mClose.setOnClickListener(mClosePreview);
+        mPermissionRequest.setOnClickListener(mPermissionListener);
     }
-
-    @Override
-    public void onImageCaptured(File file) {}
-
-    @Override
-    public void onVideoCaptured(File file) {}
 
     @Override
     public void onStart() {
         super.onStart();
-        if (mShowCamera) {
-            showCamera();
+        if (PermissionChecker.hasPermissions(getContext(), REQUIRED_PERMISSIONS)) {
+            if (mShowCamera) {
+                showCamera();
+            } else {
+                showPreview();
+            }
         } else {
-            showPreview();
+            showPermissionPrompt();
         }
     }
 
     @Override
-    public void onDestroy() {
-        super.onDestroy();
+    public void onStop() {
+        mCamera.close();
         mClassifier.close();
+        super.onStop();
+    }
+
+    private static <T> T[] concat(T[] first, T[] second) {
+        T[] result = Arrays.copyOf(first, first.length + second.length);
+        System.arraycopy(second, 0, result, first.length, second.length);
+        return result;
     }
 }
